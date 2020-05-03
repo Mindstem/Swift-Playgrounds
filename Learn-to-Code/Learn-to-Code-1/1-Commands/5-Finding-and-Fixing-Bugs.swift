d4e1e51cb644cd8eb1ecf1a0541df3b9aa3354da
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-05-03 16:40:27 -0700

    add Learn-to-Code programs

---
+++ Learn-to-Code/Learn-to-Code-1/1-Commands/5-Finding-and-Fixing-Bugs.swift
@@ -0,0 +1,11 @@
+for i in 1 ... 2 {
+    moveForward()
+}
+turnLeft()
+moveForward()
+collectGem()
+moveForward()
+toggleSwitch()
+
+
+// CODE REVIEW BY DADDY: NICE!
---
+++ Learn-to-Code/Learn-to-Code-1/2-Functions/3-Collect-Toggle-Repeat.swift
@@ -0,0 +1,21 @@
+func doBoth() {
+    moveForward()
+    collectGem()
+    moveForward()
+    toggleSwitch()
+}
+
+doBoth()
+moveForward()
+turnLeft()
+doBoth()
+moveForward()
+moveForward()
+turnLeft()
+doBoth()
+moveForward()
+turnLeft()
+doBoth()
+
+
+// CODE REVIEW BY DADDY: GOOD
