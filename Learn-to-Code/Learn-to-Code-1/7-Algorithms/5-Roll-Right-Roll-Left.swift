e9ef2695b35f00e9d08c52f6f96a7e80797529c4
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-05-04 09:18:57 -0700

    finish Learn-to-Code-1

---
+++ Learn-to-Code/Learn-to-Code-1/7-Algorithms/5-Roll-Right-Roll-Left.swift
@@ -0,0 +1,29 @@
+// my code
+func goToTheEnd() {
+    while !isBlocked {
+        moveForward()
+    }
+}
+
+for i in 1 ... 3 {
+    moveForward()
+    collectGem()
+    turnRight()
+    moveForward()
+    collectGem()
+    goToTheEnd()
+    for g in 1 ... 2 {
+        turnLeft()
+        moveForward()
+        toggleSwitch()
+    }
+    goToTheEnd()
+    turnRight()
+}
+moveForward()
+collectGem()
+turnRight()
+moveForward()
+collectGem()
+
+// CODE REVIEW BY DADDY: OUTSTANDING! :B
