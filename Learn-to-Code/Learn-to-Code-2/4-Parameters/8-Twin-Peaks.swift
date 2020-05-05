c1a0e3d54e9b8ea0d2c9a4e1ce206e731410e4a1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-04 22:21:46 -0700

    refactor program

--- Learn-to-Code/Learn-to-Code-2/4-Parameters/8-Twin-Peaks.swift
+++ Learn-to-Code/Learn-to-Code-2/4-Parameters/8-Twin-Peaks.swift
@@ -1,3 +1,5 @@
+let totalGems = randomNumberOfGems
+
 func turnAround(who: Character) {
     for i in 1 ... 2 {
         who.turnLeft()
@@ -19,28 +21,28 @@ var sams: [Character] = []
 for i in 1 ... 7 {
     let sam = Character()
     sams.append(sam)
-    world.place(sam, facing: north, atColumn: 3, row: i-1)
+    world.place(sam, facing: north, atColumn: 3, row: i - 1)
 }
 
 var foodies: [Expert] = []
 for i in 1 ... 7 {
     let foodie = Expert()
     foodies.append(foodie)
-    world.place(foodie, facing: north, atColumn: 4, row: i-1)
+    world.place(foodie, facing: north, atColumn: 4, row: i - 1)
 }
 
 var pixies: [Expert] = []
 for i in 1 ... 7 {
     let pixie = Expert()
     pixies.append(pixie)
-    world.place(pixie, facing: north, atColumn: 2, row: i-1)
+    world.place(pixie, facing: north, atColumn: 2, row: i - 1)
 }
 
 
 for i in 1 ... 7 {
-    checkTile(who: sams[i-1])
-    checkTile(who: foodies[i-1])
-    checkTile(who: pixies[i-1])
+    checkTile(who: sams[i - 1])
+    checkTile(who: foodies[i - 1])
+    checkTile(who: pixies[i - 1])
 }
 
 
