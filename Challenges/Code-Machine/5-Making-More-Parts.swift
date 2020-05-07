8f9836257922bbbc149e08ba605954304ce369ed
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-06 20:16:59 -0700

    finish Code-Machine

---
+++ Challenges/Code-Machine/5-Making-More-Parts.swift
@@ -0,0 +1,11 @@
+let items = [Item.metal, Item.stone, Item.cloth, Item.DNA]
+
+for item in items {
+    setItemA(item)
+    setItemB(.gear)
+    switchLightOn(.red)
+    forgeItems()
+}
+
+
+// CODE REVIEW: OK
