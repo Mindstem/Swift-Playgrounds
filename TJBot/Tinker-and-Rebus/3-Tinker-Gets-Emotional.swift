edd0e8dd520fd8eaad8d1221c9e580bd1a88235a
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-09 11:46:46 -0700

    add place-holder TJBot programs

---
+++ TJBot/Tinker-and-Rebus/3-Tinker-Gets-Emotional.swift
@@ -0,0 +1,12 @@
+let tinker = VirtualTJBot()
+let rebus = RebusTheBee()
+
+enum Emotion: String {
+    case anger
+    case fear
+    case joy
+    case sadness
+    case unknown
+}
+
+let story: [String] = []
---
+++ TJBot/Tinker-and-Rebus/4-Secret-Dance-of-the-TFBot.swift
@@ -0,0 +1,7 @@
+let tinker = VirtualTJBot()
+let rebus = RebusTheBee()
+
+let instructions = [
+    "D'abord, tu dois briller rouge",
+    "Emetti luce blu, poi alza il braccio"
+]
