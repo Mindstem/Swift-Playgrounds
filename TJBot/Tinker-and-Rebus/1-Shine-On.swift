a55e0cd9b13834dc097cc67280b1b1a5788fefba
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-08-24 20:54:11 -0700

    update TJBot Tinker-and-Rebus programs

--- TJBot/Tinker-and-Rebus/1-Shine-On.swift
+++ TJBot/Tinker-and-Rebus/1-Shine-On.swift
@@ -1,7 +1,16 @@
 let tinker = VirtualTJBot()
 
-tinker.shine(color: #colorLiteral(red: 0.9764705882352941, green: 0.8509803921568627, blue: 0.5490196078431373, alpha: 1.0))
+tinker.shine(
+    color: #colorLiteral(red: 0.9764705882352941,
+                         green: 0.8509803921568627,
+                         blue: 0.5490196078431373,
+                         alpha: 1.0))
 
 tinker.sleep(duration: 3)
 
-tinker.pulse(color: #colorLiteral(red: 0.9254901960784314, green: 0.23529411764705882, blue: 0.10196078431372549, alpha: 1.0), duration: 9)
+tinker.pulse(
+    color: #colorLiteral(red: 0.9254901960784314,
+                         green: 0.23529411764705882,
+                         blue: 0.10196078431372549,
+                         alpha: 1.0),
+    duration: 9)
--- TJBot/Tinker-and-Rebus/2-Wave-to-Rebus.swift
+++ TJBot/Tinker-and-Rebus/2-Wave-to-Rebus.swift
@@ -2,10 +2,19 @@ let tinker = VirtualTJBot()
 
 tinker.wave()
 
-tinker.shine(color: #colorLiteral(red: 0.3411764705882353, green: 0.6235294117647059, blue: 0.16862745098039217, alpha: 1.0))
+tinker.shine(
+    color: #colorLiteral(red: 0.3411764705882353,
+                         green: 0.6235294117647059,
+                         blue: 0.16862745098039217,
+                         alpha: 1.0))
 
 tinker.raiseArm()
 
-tinker.pulse(color: #colorLiteral(red: 0.9686274509803922, green: 0.7803921568627451, blue: 0.34509803921568627, alpha: 1.0), duration: 3)
+tinker.pulse(
+    color: #colorLiteral(red: 0.9686274509803922,
+                         green: 0.7803921568627451,
+                         blue: 0.34509803921568627,
+                         alpha: 1.0),
+    duration: 3)
 
 tinker.lowerArm()
