f1c762755ffb2a37c1a22a62b62ffaf5f7004cdb
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-08-24 23:50:44 -0700

    add TJBot 1-Tinker-and-Rebus 3-Tinker-Gets-Emotional

--- TJBot/1-Tinker-and-Rebus/3-Tinker-Gets-Emotional.swift
+++ TJBot/1-Tinker-and-Rebus/3-Tinker-Gets-Emotional.swift
@@ -1,7 +1,9 @@
 Watson.toneAnalyzer.apikey = "6GCpSGKMODqt95G95QsT27hvw704FxYcmvgaZHjdSUHd"
 
+
 let tinker = VirtualTJBot()
 let rebus = RebusTheBee()
+
 
 enum Emotion: String {
     case anger
@@ -11,4 +13,48 @@ enum Emotion: String {
     case unknown
 }
 
-let story: [String] = []
+let story: [String] = [
+    "Hello, World",
+    "I am TJBot",
+    "How are you today?"
+]
+
+
+func getPrimaryEmotion(toneResponse: ToneResponse) -> Emotion {
+    if (toneResponse.error == nil) {
+        var maxEmotion = max(toneResponse.emotion.anger,
+                             toneResponse.emotion.fear,
+                             toneResponse.emotion.joy,
+                             toneResponse.emotion.sadness)
+        
+        if (toneResponse.emotion.anger == maxEmotion) {
+            return .anger
+        } else if (toneResponse.emotion.fear == maxEmotion) {
+            return .fear
+        } else if (toneResponse.emotion.joy == maxEmotion) {
+            return .joy
+        } else {
+            return .sadness
+        }
+    } else {
+        return .unknown
+    }
+}
+
+
+tinker.shine(color: .white)
+
+for sentence in story {
+    rebus.speak(sentence)
+    
+    var toneResponse = tinker.analyzeTone(text: sentence)
+    var primaryEmotion = getPrimaryEmotion(toneResponse: toneResponse)
+    
+    switch primaryEmotion {
+        case .sadness: tinker.shine(color: .blue)
+        case .joy: tinker.shine(color: .yellow)
+        case .fear: tinker.shine(color: .purple)
+        case .anger: tinker.shine(color: .red)
+        case .unknown: break
+    }
+}
