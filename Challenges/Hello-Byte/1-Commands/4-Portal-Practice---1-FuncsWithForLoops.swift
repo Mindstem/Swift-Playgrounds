77d21ed6d6f25aeb0d6c3c71cf23b90864fcc3ea
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-15 12:13:24 -0800

    add Hello-Byte programs

---
+++ Challenges/Hello-Byte/1-Commands/4-Portal-Practice---1-FuncsWithForLoops.swift
@@ -0,0 +1,20 @@
+func move2() {
+    for i in 1 ... 2 {
+        moveForward()
+    }
+}
+
+func move3() {
+    for i in 1 ... 3 {
+        moveForward()
+    }
+}
+
+move3()
+turnLeft()
+move2()
+toggleSwitch()
+move2()
+turnLeft()
+move2()
+collectGem()
