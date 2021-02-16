a66af19dddfd7fbe0727f7b25c1710e2f0a9b222
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-15 16:05:03 -0800

    add Hello-Byte programs

---
+++ Challenges/Hello-Byte/1-Commands/4-Portal-Practice---1-Func.swift
@@ -0,0 +1,15 @@
+func move2() {
+    moveForward()
+    moveForward()
+}
+
+moveForward()
+moveForward()
+moveForward()
+turnLeft()
+move2()
+toggleSwitch()
+move2()
+turnLeft()
+move2()
+collectGem()
---
+++ Challenges/Hello-Byte/1-Commands/4-Portal-Practice---2-Funcs.swift
@@ -0,0 +1,19 @@
+func move2() {
+    moveForward()
+    moveForward()
+}
+
+func move3() {
+    moveForward()
+    moveForward()
+    moveForward()
+}
+
+move3()
+turnLeft()
+move2()
+toggleSwitch()
+move2()
+turnLeft()
+move2()
+collectGem()
