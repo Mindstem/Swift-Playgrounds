b76f0010dd175a9384635f83fa007c43c9df9f17
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-15 16:24:55 -0800

    add Hello-Byte program

---
+++ Challenges/Hello-Byte/2-Functions/3-Slotted-Stairways---0.swift
@@ -0,0 +1,21 @@
+func collectGemAndTurnAround() {
+	moveForward()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+
+func solveRow() {
+	collectGemTurnAround()
+    collectGemTurnAround()
+    turnRight()
+    moveForward()
+    turnLeft()
+}
+
+solveRow()
+solveRow()
+solveRow()
