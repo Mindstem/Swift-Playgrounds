08a3a9e91ed29d3cc110c907462ed60ff86a267e
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-18 22:40:33 -0800

    rename Commands program

--- Learn-to-Code/Learn-to-Code-1/1-Commands/7-Shortest-Route---3-FuncWithArg.swift
+++ Learn-to-Code/Learn-to-Code-1/1-Commands/7-Shortest-Route---3-FuncWithArg.swift
@@ -1,13 +1,12 @@
+// funcs
 func go(d: Int) {
     for i in 1 ... d {
         moveForward()
     }
 }
 
+// main
 go(d: 3) 
 collectGem()
 go(d: 4)
 toggleSwitch()
-
-
-// CODE REVIEW BY DADDY: GOOD
