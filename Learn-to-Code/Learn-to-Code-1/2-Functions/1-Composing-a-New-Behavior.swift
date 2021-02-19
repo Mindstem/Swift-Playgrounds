679a7f5df90534fcec3e6e8ee01f04ef360818a7
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-18 21:22:47 -0800

    update Functions program

--- Learn-to-Code/Learn-to-Code-1/2-Functions/1-Composing-a-New-Behavior.swift
+++ Learn-to-Code/Learn-to-Code-1/2-Functions/1-Composing-a-New-Behavior.swift
@@ -1,19 +1,10 @@
-func go(dist: Int) {
-    for i in 1 ... dist {
-        moveForward()
-    }
-}
-
-func turnRight() {
-    for i in 1 ... 3 {
-        turnLeft()
-    }
-}
-
-go(dist: 3) 
-turnRight()
-go(dist: 3)
+moveForward()
+moveForward()
+moveForward()
+turnLeft()
+turnLeft()
+turnLeft()
+moveForward()
+moveForward()
+moveForward()
 collectGem()
-
-
-// CODE REVIEW BY DADDY: GOOD!
