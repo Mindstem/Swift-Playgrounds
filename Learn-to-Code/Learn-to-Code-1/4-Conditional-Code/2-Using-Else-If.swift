cd38951279a33d07c5b44c71ddac5b1fa926b2d6
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-18 20:38:03 -0800

    update Learnt-to-Code-1 programs

--- Learn-to-Code/Learn-to-Code-1/4-Conditional-Code/2-Using-Else-If.swift
+++ Learn-to-Code/Learn-to-Code-1/4-Conditional-Code/2-Using-Else-If.swift
@@ -1,8 +1,8 @@
 for i in 1 ... 2 {
     moveForward()
-    if isOnClosedSwitch {
-        toggleSwitch()
-    } else if isOnGem {
+    if isOnGem {
         collectGem()
+    } else if isOnClosedSwitch {
+        toggleSwitch()
     }
 }
