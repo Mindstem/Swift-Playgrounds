fd4ec5cb32cbe80c3cff5f5e9770329be60d219a
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-18 22:05:55 -0800

    update For-Loops program

--- Learn-to-Code/Learn-to-Code-1/3-For-Loops/4-Loop-Jumper.swift
+++ Learn-to-Code/Learn-to-Code-1/3-For-Loops/4-Loop-Jumper.swift
@@ -1,13 +1,9 @@
-for i in 1 ... 5{
+for i in 1 ... 5 {
     moveForward()
     turnLeft()
-    for a in 1 ... 2 {
+    for i in 1 ... 2 {
         moveForward()
     }
     collectGem()
     turnRight()
 }
-
-
-// CODE REVIEW BY DADDY: GREAT!
-// - Antoni remembered to use a different iterator ("a") inside the outer loop which uses "i" :)
