e3af3140c5eec9085012819249c5f74716783091
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-02-18 23:16:58 -0800

    add For-Loops programs

--- Learn-to-Code/Learn-to-Code-1/3-For-Loops/2-Looping-All-the-Sides.swift
+++ Learn-to-Code/Learn-to-Code-1/3-For-Loops/2-Looping-All-the-Sides.swift
@@ -1,11 +1,11 @@
 for i in 1 ... 4 {
     moveForward()
     collectGem()
-    for t in 1 ... 3 {
+
+    // to be sure, use a different iterator ("j") to the outer loop iterator "i"
+    for j in 1 ... 3 {
         moveForward()
     }
+
     turnRight()
 }
-
-
-// CODE REVIEW BY DADDY: NICE! :)
