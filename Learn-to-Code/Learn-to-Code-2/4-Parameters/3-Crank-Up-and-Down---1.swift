fff3fc5ffda4590d57faeb619107ebd6d4cdaa43
Ariana Luong-Pham <Aria.LuongPham@GMail.com>
2021-03-08 23:40:04 -0800

    refactor Parameters programs

--- Learn-to-Code/Learn-to-Code-2/4-Parameters/3-Crank-Up-and-Down---1.swift
+++ Learn-to-Code/Learn-to-Code-2/4-Parameters/3-Crank-Up-and-Down---1.swift
@@ -18,6 +18,8 @@ expert.turnLock(up: true, numberOfTimes: 4)
 character.turnRight()
 character.move(distance: 2)
 character.collectGem()
+
+// 3rd gem
 turnAround(who: character)
 character.moveForward()
 expert.turnLock(up: false, numberOfTimes: 3)
@@ -25,38 +27,39 @@ character.turnRight()
 character.moveForward()
 character.collectGem()
 
-// 3rd gem
-turnAround(who: character)
-character.move(distance: 2)
-character.turnLeft()
+// 4th gem
 expert.turnRight()
 expert.turnLockUp()
-character.move(distance: 2)
-character.collectGem()
-
-// 4th gem
 turnAround(who: character)
 character.move(distance: 2)
 character.turnLeft()
-character.move(distance: 2)
-turnAround(who: expert)
-expert.turnLockUp()
 character.move(distance: 2)
 character.collectGem()
 
 // 5th gem
+expert.turnRight()
+expert.turnLockUp()
 turnAround(who: character)
 character.move(distance: 2)
-character.turnRight()
-expert.turnLeft()
-expert.turnLockUp()
+character.turnLeft()
+character.move(distance: 2)
+character.turnLeft()
 character.move(distance: 2)
 character.collectGem()
 
 // 6th gem
+expert.turnRight()
+expert.turnLockUp()
+turnAround(who: character)
+character.move(distance: 2)
+character.turnLeft()
+character.move(distance: 2)
+character.collectGem()
+
+// 7th gem
 turnAround(who: character)
 character.moveForward()
 expert.turnLock(up: true, numberOfTimes: 3)
-character.turnLeft()
+character.turnRight()
 character.moveForward()
 character.collectGem()
